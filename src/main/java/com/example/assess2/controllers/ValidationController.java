d97c1f114be90df04462f32b48411eab316002cd
unknown <kovartravis@gmail.com>
2017-11-28 15:26:35 -0600

    added endpoint to validation

--- src/main/java/com/example/assess2/controllers/ValidationController.java
+++ src/main/java/com/example/assess2/controllers/ValidationController.java
@@ -3,6 +3,7 @@ package com.example.assess2.controllers;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
