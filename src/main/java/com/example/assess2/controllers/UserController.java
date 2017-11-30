ee2622c1fa97c2764e2f2313ab957313fa26ef99
unknown <kovartravis@gmail.com>
2017-11-30 10:43:08 -0600

    bug fix

--- src/main/java/com/example/assess2/controllers/UserController.java
+++ src/main/java/com/example/assess2/controllers/UserController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -185,7 +184,7 @@ public class UserController {
 		}
 	}
 	
-	@DeleteMapping("/@{username}")
+	@PostMapping("/@{username}")
 	public UserDto deleteUser (@PathVariable String username, @RequestBody Credentials credentials, HttpServletResponse response) throws IOException {
 	    try {
 			return userService.deleteUser(username, credentials);
